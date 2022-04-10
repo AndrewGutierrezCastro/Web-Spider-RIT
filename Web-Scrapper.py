0335819af54ef41b6b2598d46bccb6a4be4b16d6
AndrewGutierrezCastro <andrewgc@estudiantec.cr>
2022-04-10 13:53:26 -0600

    Web spider without thread and scheduler.

--- Web-Scrapper.py
+++ Web-Scrapper.py
@@ -1,52 +1,53 @@
 from bs4 import BeautifulSoup as soup  # Lector HTML
 from urllib.request import urlopen as uReq  # Web client
 
-def guardarArchivo(datos,encabezado,archivo):
+def guardarArchivo(datos,archivo):
     for dato in datos:
-        informacion = dato.find("a",class_="item-title").text.split(' ',1)
-        try:
-            info_producto = dato.findAll("div", {"class": "item-info"})
-            info_precio = info_producto[0].findAll("ul",{"class":"price"})
-            if(len(info_precio) > 0):
-                precio = info_precio[0].findAll("li",{"class":"price-current"})[0].findAll("strong")[0].text
-            else:
-                info_producto = dato.findAll("div", {"class": "item-action"})
-                precio = info_producto[0].findAll("ul",{"class":"price"})[0].findAll("li",{"class":"price-current"})[0].findAll("strong")[0].text
-        except:
-            precio = 'NA'
-        marca = informacion[0]
-        desc_producto = informacion[1]
-        pos_gb = informacion[1].find("GB")
-        cant_ram = informacion[1][pos_gb-1]
-        if(not(cant_ram.isdigit())):
-            cant_ram = "0"
-        print("Marca: " + marca + "\n")
-        print("Desc_Producto: " + desc_producto + "\n")
-        print("Cant_Ram: " + cant_ram + "\n")
-        print("Precio: " + precio + "\n")
-        archivo.write(marca + ", " + desc_producto.replace(",", "|") + ", " + cant_ram +", " +precio.replace(",", "") +"\n")
+        informacionParseada = parser(dato)
+        archivo.write(informacionParseada)
+
+def parser(dato):
+    informacion = dato.find("a",class_="item-title").text.split(' ',1)
+    try:
+        info_producto = dato.findAll("div", {"class": "item-info"})
+        info_precio = info_producto[0].findAll("ul",{"class":"price"})
+        if(len(info_precio) > 0):
+            precio = info_precio[0].findAll("li",{"class":"price-current"})[0].findAll("strong")[0].text
+        else:
+            info_producto = dato.findAll("div", {"class": "item-action"})
+            precio = info_producto[0].findAll("ul",{"class":"price"})[0].findAll("li",{"class":"price-current"})[0].findAll("strong")[0].text
+    except:
+        precio = 'NA'
+    marca = informacion[0]
+    desc_producto = informacion[1]
+
+    print("Marca: " + marca + "\n")
+    print("Desc_Producto: " + desc_producto + "\n")
+    print("Precio: " + precio + "\n")
+    resultado = marca + ", " + desc_producto.replace(",", "|") + ", " +precio.replace(",", "") +"\n"
+    return resultado
     
 
-def scrapper(url,cant_paginas):
+def scrapper(url,cant_paginas, nombre_archivo):
     acum = 1
-    archivo_guardado = "articulos.csv"
-    encabezado = "Marca,Desc_Producto,Cant_Ram,Precio\n"
-    archivo = open(archivo_guardado, "w")
+    encabezado = "Marca,Desc_Producto,Precio\n"
+    archivo = open(nombre_archivo, "w+")
+    archivo.write(encabezado)
     while (acum <= cant_paginas):
         url = url + '&page='+str(acum)
         uClient = uReq(url)
         sopa_html = soup(uClient.read(), "html.parser")
         uClient.close()
         datos = sopa_html.findAll("div", {"class": "item-container"})
-        guardarArchivo(datos,encabezado,archivo)
+        guardarArchivo(datos,archivo)
         acum = acum+1
         print("Leyendo Pagina # "+str(acum)+ " de "+str(cant_paginas))
     archivo.close()
     
 
-def main():
-    url = "https://www.newegg.com/p/pl?d=GPU"
-    scrapper(url,100)
+def request(item_name, amount_pages, nombre_archivo):
+    url = "https://www.newegg.com/p/pl?d="+item_name
+    scrapper(url,amount_pages,nombre_archivo)
 
 if __name__ == '__main__':
-    main()
+    request("GPU",10,"GPU.csv")
