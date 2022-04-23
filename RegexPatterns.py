8e9f4f699bca64c33954885d2681dba5ca979406
fergo04 <88514940+fergo04@users.noreply.github.com>
2022-04-23 16:37:15 -0600

    Update RegexPatterns.py
    HDD y PSU patterns

--- RegexPatterns.py
+++ RegexPatterns.py
@@ -2,3 +2,5 @@
 CASEPATTERN = r"Full.tower|Mid.tower|Micro.tower|Mini.tower|Micro.atx|Mini.atx|atx|tempered.glass"
 CPUPATTERN =  r"celeron.\w+|i3|i5|i7|core.\d.duo|core.\d.quad|\d.\d.Ghz|LGA.\d{3,4}|\d{2,4}w|\w+.core|\d\d?...gen|\w+.lake|\d.\dghz|turbo.boost|ryzen.\d|\d{4}x?|\w+.boost|\w+.lake|\d{1,3}mm|cooler|desktop|\w\w\d{3}|cpu.fan|core.i\d.\d{4,5}\w?|\w+.\w+.graphics|socket.\w+|zen.\d|..series|CPU.\w+.fan|for.\w+|rgb"
 GPUPATTERN = r"amd|nvidia|\d{1,3}gb|gddr\dx?|pci.express.\d.\d|gt.?\d{1,4}|geforce|gtx.?\d{2,4}.?t?i?|radeon|rx.?\d{2,4}.?x?t?|rtx.?\d{4}.?t?i?|x\d\d|crossfirex|sli|quadro.?\w\d{3,5}|m\w{3,5}..tx|\w+.?fan|dual.?\w+|support.\w+|\w+.?profile|\w.?sync|gaming|\d{2,4}.?bit"
+HDDPATTERN = r"([0-9]+[.])*[0-9]{1,9}(tb|mb.cache|gb.s|.rpm)"
+PSUPATTERN = r"[0-9]{1,9}.w|(gold|silver|platinum|titanium|bronze)"
