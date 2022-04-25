b563373b9935f99cb7ee4929e78c37ca2882b1d4
AndrewGutierrezCastro <andrewgc@estudiantec.cr>
2022-04-25 11:09:21 -0600

    Preprocessing TODO's made it, RAM pattern is missing

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,39 +1,43 @@
 import re
+import json
+
+from attr import has
 import RegexPatterns
 import pandas as pd
-from nltk.tokenize import sent_tokenize, word_tokenize
 
-FILENAME = ["CASE","CPU","GPU","HDD","MotherBoard","PSU","RAM","SSD"]
+FILENAME = {"CASE":RegexPatterns.CASEPATTERNATTERN,
+            "CPU":RegexPatterns.CPUPATTERNTERN,
+            "GPU":RegexPatterns.GPUPATTERNATTERN,
+            "HDD":RegexPatterns.HDDPATTERNDPATTERN,
+            "MotherBoard":RegexPatterns.MOTHERBOARDBPATTERN,
+            "PSU":RegexPatterns.PSUPATTERNTERN,
+            "RAM":RegexPatterns.RAMPATTERN,
+            "SSD":RegexPatterns.SSDPATTERN}
 FILETYPE = ".csv"
 
 hashmapData = {}
-
-def tokenizer(string):
-    return word_tokenize(string)
 
 def denoise_text(stringWords, regexPattern):
     return re.findall(regexPattern, stringWords, re.IGNORECASE)
 
 def load_info():
     global hashmapData
-    for name in FILENAME:
+    for name in FILENAME.keys():
         data = pd.read_csv(name + FILETYPE)
         hashmapData[name] = data
     
 
 def preprocessing():
     global hashmapData
-    #load_info() TODO check fields and content on csv files missmatching 
-    # pandas.errors.ParserError: Error tokenizing data. C error: Expected 4 fields in line 529, saw 5
-    #TODO denoise_text for each file
-    data = pd.read_csv("GPU.csv")
-    descriptionList = data.Desc_Producto.tolist()
-    #print(descriptionList[:5])
-    for i in descriptionList:
-        
-        result = denoise_text(i, RegexPatterns.GPUPATTERN)
-        print(result)
+    for name in hashmapData.keys():
+        data = hashmapData[name]
+        descriptionList = data.Desc_Producto.tolist()
+        for i in descriptionList:
+            result = denoise_text(i, FILENAME[name])
+            print(result)
 
 if __name__ == '__main__':
     print("Preprocessing version 0.1 running....")
+    load_info()
     preprocessing()
+    print(json.dumps(hashmapData, sort_keys=True, indent=4))
