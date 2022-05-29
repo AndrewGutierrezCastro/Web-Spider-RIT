81ed1f9e7747c3e9a47f57893592dc34d151818e
christianacuna <christianlorenso@hotmail.com>
2022-05-29 13:02:30 -0600

    added documentation part 1

--- Calculo.py
+++ Calculo.py
@@ -13,9 +13,22 @@ def calc_idf_hard(list_resultados,extencion):
 def calc_peso(tipo_idf, tipo_tf, freq,maxi,):
     return calc_idf() * calc_tf()
 
+'''
+    Funcion de calculo tf, el cual da la importancia del termino en el documento
+    Tipo: binario, logaritmica, doble, k, frecuencia
+    Frequencia: La frequencia de un termino en un documento
+    Args: tipo(String), frequencia(Int)
+'''
 def calc_tf(tipo,freq):
     return calc_tf(tipo,freq,1)
 
+'''
+    Funcion de calculo tf, el cual da la importancia del termino en el documento
+    Tipo: binario, logaritmica, doble, k, frecuencia
+    Frequencia: La frequencia de un termino en un documento
+    K: Una constante cualquierda
+    Args: tipo(String), frequencia(Int), k(Int)
+'''
 def calc_tf(tipo,freq,k):
     if tipo =="binario" :
         resultado = calc_tf_bin(freq)
@@ -29,36 +42,77 @@ def calc_tf(tipo,freq,k):
         resultado = calc_tf_freq(freq)
     return resultado
 
+'''
+    Funcion de calculo tf Binaria, el cual da la importancia del termino en el documento
+    Frequencia: La frequencia de un termino en un documento
+    Args: frequencia(Int)
+'''
 def calc_tf_bin(freq):
-    resultado = 0
+    if freq > 0:
+        resultado = 1
+    else:
+        resultado = 0
     return resultado
-
+'''
+    Funcion de calculo tf Frequencia, el cual da la importancia del termino en el documento
+    Frequencia: La frequencia de un termino en un documento
+    Args: frequencia(Int)
+'''
 def calc_tf_freq(freq):
     resultado = freq
     return resultado
-
+'''
+    Funcion de calculo tf Normalizacion Logaritmica, el cual da la importancia del termino en el documento
+    Frequencia: La frequencia de un termino en un documento
+    Args: frequencia(Int)
+'''
 def calc_tf_log(freq):
     resultado = 1 + np.log2(freq)
     return resultado
 
+'''
+    Funcion de calculo tf Normalizacion Doble 0.5, el cual da la importancia del termino en el documento
+    Frequencia: La frequencia de un termino en un documento
+    Max(i): Cantidad de terminos en un documento
+    Args: frequencia(Int), maxi(Int)
+'''
 def calc_tf_double(freq,maxi):
     resultado = 0.5+0.5 * (freq/ ( maxi * freq ))
     return resultado
-
+'''
+    Funcion de calculo tf Normalizacion Doble 0.5, el cual da la importancia del termino en el documento
+    Frequencia: La frequencia de un termino en un documento
+    Max(i): Cantidad de terminos en un documento
+    K: Una constante cualquierda
+    Args: frequencia(Int), maxi(Int), K(Int)
+'''
 def calc_tf_double_k(freq,maxi,k):
     resultado = k + (1-k) * ( freq / ( maxi * freq ))
     return resultado
 
-
+'''
+    Funcion de calculo idf, el cual da la especifidad del termino en la coleccion
+    Tipo: unaria, suavizada, maxima, probabilistica, inversa
+    Lista_Documentos: una lista con todos los documentos en la coleccion.
+    Lista_Documentos_Encontrados: una lista con los documentos donde aparece el termino
+    Args: tipo(String), lista_documentos(List),lista_documentos_encontrados(List)
+'''
 def calc_idf(tipo,list_docus,lista_docus_encontrado):
     return calc_idf(tipo,list_docus,lista_docus_encontrado,1)
 
+
+'''
+    Funcion de calculo idf, el cual da la especifidad del termino en la coleccion
+    Tipos: unaria,suavizada,maxima,probabilistica,inversa
+    Lista_Documentos: una lista con todos los documentos en la coleccion.
+    Lista_Documentos_Encontrados: una lista con los documentos donde aparece el termino
+    Cant_Terminos: es la cantidad de terminos en un documento
+    Args: tipo(String), lista_documentos(List),lista_documentos_encontrados(List),cantidad_terminos(Int)
+'''
 def calc_idf(tipo,list_docus,lista_docus_encontrado,cant_terminos):
-    
     cant_documentos = len(list_docus)
     cant_doccumentos_encontrado = len(lista_docus_encontrado)
     maxi = cant_terminos
-
     if tipo =="unaria" :
         resultado = calc_idf_unaria()
     elif tipo == "suavizada":
@@ -70,25 +124,53 @@ def calc_idf(tipo,list_docus,lista_docus_encontrado,cant_terminos):
     else:
         resultado = calc_idf_inversa(cant_documentos,cant_doccumentos_encontrado)
     return resultado
-
+'''
+    Funcion IDF Unaraia, donde siempre retorna 1
+'''
 def calc_idf_unaria():
     resultado = 1
     return resultado
 
+'''
+    Funcion IDF de Frequencia Inversa.
+    Cantidad_Documentos: La cantidad de documentos en la coleccion.
+    Cantidad_Documentos_Encontrados: La cantidad de documentos donde aparece el termino.
+    Args:Cantidad_Documentos(Int), Cantidad_Documentos_Encontrados(Int)
+'''
 def calc_idf_inversa(cant_documentos,cant_doccumentos_encontrado):
     resultado = np.log2( cant_documentos / cant_doccumentos_encontrado )
     return resultado
 
+'''
+    Funcion IDF de Frequencia Inversa Suavizada.
+    Cantidad_Documentos: La cantidad de documentos en la coleccion.
+    Cantidad_Documentos_Encontrados: La cantidad de documentos donde aparece el termino.
+    Args:Cantidad_Documentos(Int), Cantidad_Documentos_Encontrados(Int)
+'''
 def calc_idf_inversa_suavi(cant_documentos,cant_doccumentos_encontrado):
     resultado = np.log2(1 + ( cant_documentos / cant_doccumentos_encontrado ))
     return resultado
 
+'''
+    Funcion IDF de Frequencia Inversa Maxima.
+    Cantidad_Documentos_Encontrados: La cantidad de documentos donde aparece el termino.
+    Max(i): La cantidad de terminos 
+    Args:Cantidad_Documentos(Int), Cantidad_Documentos_Encontrados(Int)
+'''
 def calc_idf_inversa_max(cant_doccumentos_encontrado,maxi):
     resultado = np.log2(1 + ((( cant_doccumentos_encontrado * maxi) / cant_doccumentos_encontrado )))
     return resultado
 
+'''
+    Funcion IDF de Frequencia Inversa Probabilistica.
+    Cantidad_Documentos: La cantidad de documentos en la coleccion.
+    Cantidad_Documentos_Encontrados: La cantidad de documentos donde aparece el termino.
+    Args:Cantidad_Documentos(Int), Cantidad_Documentos_Encontrados(Int)
+'''
 def calc_idf_inversa_prob(cant_documentos,cant_doccumentos_encontrado):
     resultado = np.log2(( cant_documentos - cant_doccumentos_encontrado ) / cant_doccumentos_encontrado )
     return resultado
+
+
 
 print(calc_idf_hard([2,3,4],"csv"))
