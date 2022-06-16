76f2a80d8e43e62f61b3f0a43e214b1309c71e16
Andrew Gutierrez Castro <andrewjosegutierrezcastro@gmail.com>
2022-06-16 13:05:49 -0600

    Create searcher.py

---
+++ searcher.py
@@ -0,0 +1,8 @@
+#Authors: Andrew JGC Fernando G, Christian A.
+
+
+def query(query):
+    #query es un string por ejmplo "ATX MID TOWER"
+    #TODO Se debe hacer la consulta y retornar, los 20 documentos mas relevantes, sus textos(documents.json), su info completa (documentsFullData.json)
+
+    return [], []
